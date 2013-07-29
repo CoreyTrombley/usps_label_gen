7936521e80f861afdc0eda6dbe717bb42c62f94c
Corey Trombley <corey.trombley33@gmail.com>
2013-07-29 09:52:08 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UspsLabelGen::Application.config.secret_token = '272bb1ff4fb95d60ca3b02dbbbf74cbdd45fd5a229a3444bb0d548f548c6af21cd284d495604af7f231384dbb338a38e02fd95b3a81d3179789972d1f0177784'
